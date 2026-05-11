b29566ff712a948cb4a615e36c529765c70f8500
Elifsylmz <elfsemaylmz7@gmail.com>
2026-05-11 13:25:11 +0300

    fixed: env, expander (mini bugs)

--- env/env_init.c
+++ env/env_init.c
@@ -1,24 +1,27 @@
 #include "env.h"
 
-t_env	*env_init(char **envp)
+t_env   *env_init(char **envp)
 {
-	t_env	*list;
-	char	*eq;
-	char	*key;
-	char	*val;
-	int		i;
+    t_env   *list;
+    char    *eq;
+    char    *key;
+    char    *val;
+    int     i;
  
-	list = NULL;
-	i = 0;
-	while (envp[i])
-	{
-		eq = ft_strchr(envp[i], '=');
-		key = ft_substr(envp[i], 0, eq - envp[i]);
-		val = ft_strdup(eq + 1);
-		env_set(&list, key, val);
-		free(key);
-		free(val);
-		i++;
-	}
-	return (list);
+    list = NULL;
+    i = 0;
+    while (envp && envp[i])
+    {
+        eq = ft_strchr(envp[i], '=');
+        if (eq)
+        {
+            key = ft_substr(envp[i], 0, eq - envp[i]);
+            val = ft_strdup(eq + 1);
+            env_set(&list, key, val);
+            free(key);
+            free(val);
+        }
+        i++;
+    }
+    return (list);
 }
--- executor/expander/expander.h
+++ executor/expander/expander.h
@@ -1,13 +1,11 @@
 #ifndef EXPANDER_H
 # define EXPANDER_H
 
-# include "../../types.h"
 # include "../../minishell.h"
-# include "../../lexer/lexer.h"
-# include "../../parser/ast.h"
 # include <stdlib.h>
 # include <unistd.h>
 
-void	expand_ast(t_shell *shell, t_ast *cmd);
+void    expand_ast(t_shell *shell, t_ast *cmd);
+char    *expand_str(t_shell *shell, char *str); // Dışarıya açıldı
 
 #endif
