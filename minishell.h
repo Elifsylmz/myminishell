57a5fd50dc87a595604085fecb3afcb24f9e1679
Elifsylmz <elfsemaylmz7@gmail.com>
2026-05-11 08:38:23 +0300

    include circular durumunu kırmak için oluşturuduğum yapının sorunu kalmadı

--- minishell.h
+++ minishell.h
@@ -1,21 +1,18 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# include "types.h"
 
 # include "lexer/lexer.h"
 # include "libft/libft.h"
 # include "env/env.h"
 # include "parser/ast.h"
 
-extern int	g_signal;
-
-typedef struct s_shell
-{
-	t_token	*lex;
-	t_env	*env;
-	t_ast	*ast;
-	int		last_exit_code;
-}	t_shell;
+extern int g_signal;
 
 void	start_interactive_signals(void);
 void	start_execution_signals(void);
