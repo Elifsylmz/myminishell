1851479eea2fff0704e44c8177f6dae9c5c8364b
Elifsylmz <elfsemaylmz7@gmail.com>
2026-05-14 03:03:55 +0300

    unset problem fixed

--- executor/builtins/unset.c
+++ executor/builtins/unset.c
@@ -4,7 +4,7 @@ static int	is_valid_identifier(char *s)
 {
 	int	i;
 
-	if (!s)
+	if (!s || !s[0])
 		return (0);
 	if (!ft_isalpha(s[0]) && s[0] != '_')
 		return (0);
@@ -18,30 +18,16 @@ static int	is_valid_identifier(char *s)
 	return (1);
 }
 
-static void	unset_error(char *arg)
-{
-	ft_putstr_fd("minishell: unset: `", 2);
-	ft_putstr_fd(arg, 2);
-	ft_putstr_fd("': not a valid identifier\n", 2);
-}
-
 int	builtin_unset(t_shell *shell, char **argv)
 {
 	int	i;
-	int	status;
 
 	i = 1;
-	status = 0;
 	while (argv[i])
 	{
 		if (is_valid_identifier(argv[i]))
 			env_unset(&shell->env, argv[i]);
-		else
-		{
-			unset_error(argv[i]);
-			status = 1;
-		}
 		i++;
 	}
-	return (status);
+	return (0);
 }
